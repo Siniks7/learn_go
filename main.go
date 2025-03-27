3946107803ed0128acdb9fec3cc61786ee711ddb
Nikolay <siniks7@yandex.ru>
2025-03-27 11:17:30 +0700

    Текущая погода, дебаг

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"demo/weather/geo"
+	"demo/weather/weather"
 	"flag"
 	"fmt"
 )
 
 func main() {
 	city := flag.String("city", "", "Город пользователя")
-	// format := flag.Int("format", 1, "Формат вывода погоды")
-
+	format := flag.Int("format", 1, "Формат вывода погоды")
 	flag.Parse()
-
-	fmt.Println(*city)
 	geoData, err := geo.GetMyLocation(*city)
 	if err != nil {
 		fmt.Println(err.Error())
+	} else {
+		weatherData := weather.GetWeather(*geoData, *format)
+		fmt.Println(weatherData)
 	}
-	fmt.Println(geoData)
 }
